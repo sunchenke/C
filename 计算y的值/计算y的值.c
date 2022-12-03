d8ac9baec8267d722207fdb8b2ba0341570159a8
sunchenke <3201038603@qq.com>
2022-12-03 15:43:19 +0800

    入门程序

---
+++ 计算y的值/计算y的值.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+	//int a = 0;
+	//scanf("%d", &a);
+	//if (a < 0)
+	//{
+		//printf("1");
+	//}
+	//else if (a == 0)
+	//{
+	//	printf("0");
+	//}
+	//else if (a > 0)
+	//{
+		//printf("-1");
+	//}
+	//return 0;
+//}
+
+#include <stdio.h>
+//int main()
+//{
+	//int a = 0;
+	//scanf("%d", &a);
+	//if (a >= 140)
+	//{
+		//printf("Genius\n");
+	//}
+	//return 0;
+//}
+
+//#include <stdio.h>
+
+//int main()
+//{
+	//int a = 40, c = 212;
+	//printf("%d\n", (-8 + 22) * a - 10 + c / 2);
+
+	//return 0;
+//}
+
+
