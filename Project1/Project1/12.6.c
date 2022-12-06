b6a210ebbb09c882ec65765aeb588ba88807b1f3
sunchenke <3201038603@qq.com>
2022-12-06 13:52:21 +0800

    12.6

---
+++ Project1/Project1/12.6.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    scanf("%d %d", &a, &b);
+//    printf("%d %d", a / b, a % b);
+//    return 0;
+//}
+
+//#include <stdio.h>
+//
+//float hi = 0.0;
+//int i = 0;
+//float sum = 0.0;
+//int main()
+//{
+//	while (i < 5)
+//	{
+//		scanf("%f", &hi);
+//		sum = sum + hi;
+//		i++;
+//
+//	}
+//	printf("%.2f",sum/5);
+//		return 0;
+//}
+
+//#include <stdio.h>
+//int a = 0;
+//int max = 0;
+//int i = 0;
+//
+//int main()
+//{
+//	while (i < 4)
+//	{
+//		scanf("%d",&a);
+//		if (max < a)
+//		{
+//			max = a;
+//		}
+//		i++;
+//	}
+//	printf("%d",max);	
+//	return 0;
+//}
+
---
+++ Project2/Project2/12_6.c
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int a, b;
+//int main()
+//{
+//	while (scanf("%d %d", &a, &b) != EOF)
+//	{
+//		if (a > b)
+//		{
+//			printf("%d>%d",a,b);
+//		}
+//		else if (a < b)
+//		{
+//			printf("%d<%d", a, b);
+//		}
+//		else
+//		
+//			printf("%d=%d", a, b);
+//		
+//	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int a = 0;
+//int main()
+//{
+//	while (scanf("%d\n", &a) != EOF)
+//	{
+//		if (a % 2 == 0)
+//			printf("Even");
+//		else
+//			printf("Odd");
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int a = 0;
+//int i = 0;
+//int main()
+//{
+//	while (scanf("%d", &a) != EOF)
+//	{
+//		for (i = 0; i < a; i++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int score[5] = { 0 };
+//int i = 0;
+//float avg;
+//int sum = 0;
+//int main()
+//{
+//    for (i = 0; i < 5; i++)
+//    {
+//        scanf("%d", &score[i]);
+//        sum = sum + score[i];
+//    }
+//    avg = sum / 5.0;
+//    printf("%.1f", avg);
+//    return 0;
+//}
