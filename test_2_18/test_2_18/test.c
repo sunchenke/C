83f920b642f283e98367024f11490e0f3cb34ed2
sunchenke <3201038603@qq.com>
2023-02-18 08:18:30 +0800

    有序序列合并

---
+++ test_2_18/test_2_18/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//    int a, b, c[100], temp;
+//    scanf("%d", &a);
+//    scanf("%d", &b);
+//    for (int i = 1; i <= a + b; i++) {
+//        scanf("%d", &c[i]);
+//    }
+//    for (int i = 1; i <= a + b; i++)
+//    {
+//        for (int j = 1; j <= a + b - i; j++)
+//        {
+//            if (c[j] > c[j + 1])
+//            {
+//                temp = c[j];
+//                c[j] = c[j + 1];
+//                c[j + 1] = temp;
+//            }
+//        }
+//    }
+//    for (int i = 1; i < a + b + 1; i++) {
+//        printf("%d ", c[i]);
+//    }
+//    return 0;
+//}
+
